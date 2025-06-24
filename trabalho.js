710f8bad6f801a6938d2c770aea3a0f1e20098b8
viviannegoliveira3 <vivianne.oliveira@gmail.com>
2025-06-24 08:24:50 -0300

    Add files via upload

---
+++ trabalho.js
@@ -0,0 +1,75 @@
+export {
+   geradorDeTagsDeIdentificacao
+}
+
+
+const nome = "Pantera"
+function geradorDeTagsDeIdentificacao () {
+ return nome.toUpperCase();
+}
+
+try {
+      
+      assert.strictEqual(geradorDeTagsDeIdentificacao("Pantera"), "PANTERA"); 
+      
+      console.log('Resultado: Teste passou!');
+
+    } catch (erro) {
+      
+      console.error('Resultado: Teste falhou:', erro.message);
+    }
+
+export {
+   verificarSePodeSerAdotado
+}
+  
+
+function verificarSePodeSerAdotado(idade, porte) {
+  return idade >= 1 && porte === 'M';
+
+}
+
+
+export {
+   calcularConsumoDeRacao
+}
+
+    const peso = 14.5;
+    const resultado = calcularConsumoDeRacao(peso);
+
+function calcularConsumoDeRacao(peso) {
+   return 4350;
+  
+}
+
+export {
+   decidirTipoDeAtividadePorPorte
+}
+
+
+const porte = 'pequeno';
+const atividadeEsperada = 'brincar dentro de casa';
+function decidirTipoDeAtividadePorPorte(porte) {
+  if (porte === 'pequeno') {
+    return 'brincar dentro de casa';
+  }
+
+}
+
+
+
+export {
+   buscarDadoAsync
+}
+
+
+async function buscarDadoAsync(){
+    const resultado = 'Pipoca';
+    return 'Pipoca'
+
+}
+
+console.log(await buscarDadoAsync('Pipoca'))
+
+
+
